7f3638cb57fab38bae85518cb421aaa8005b6179
Artyom767 <111982037+Artyom767@users.noreply.github.com>
2022-08-26 21:31:31 +0400

    lesson1

---
+++ main.js
@@ -0,0 +1,27 @@
+// const map =function(val,func){
+// const newArr = [];
+// for(let i=0; i<val.length; i++){ 
+  
+//     newArr[i]=func(val[i]);
+    
+// };
+// return newArr;
+// };
+
+// const res = map([1,2,3,4,5,6,7],function(val){return val*2;});
+// console.log(res)
+
+const filter = function(val){
+    let j=0;
+    const newArr = [];
+    for(let i=0; i < val.length; i++) { 
+       if(val[i] % 2 === 0) {
+         newArr[j] = val[i];
+         j++;
+       };
+    };
+    return newArr;
+};
+
+ let a = filter([1,2,3,4,5]);
+ console.log(a);
